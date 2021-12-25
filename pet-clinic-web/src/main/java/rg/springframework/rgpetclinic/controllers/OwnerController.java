00b3fc7eee79e12ea17b0857e2b6ecfe4ba3ca4e
rg <ramilgojayev2003@gmail.com>
2021-12-25 22:36:04 +0100

    Added Owners Index Page and Controller. Closes #14.

---
+++ pet-clinic-web/src/main/java/rg/springframework/rgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package rg.springframework.rgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listVets()
+    {
+        return "owners/index";
+    }
+
+}
